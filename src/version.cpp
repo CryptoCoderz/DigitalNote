14394ca8b849025850c96097945c5471aeedfc4b
CryptoCoderz <CryptoCoderz@gmail.com>
2019-03-31 01:13:49 -0700

    Go-Live Updates 2
    Changelog
    
    - Version bump for go-live payment activation

--- src/version.cpp
+++ src/version.cpp
@@ -36,7 +36,7 @@ const std::string CLIENT_NAME("DigitalNoteoshi");
 // git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#    define GIT_COMMIT_ID "XDN"
+#    define GIT_COMMIT_ID "XDN-"
 #endif
 
 #define BUILD_DESC_FROM_COMMIT(maj,min,rev,build,commit) \
